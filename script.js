a81bd645aa348457bcd565112bb36f4f39737ec4
Irvin Ruiz <irvinruiz44@yahoo.com>
2021-09-26 18:37:35 -0600

    added js questions

--- script.js
+++ script.js
@@ -6,3 +6,38 @@ const progressBarrFull = document.querySelector('#progressBarFull')
 
 let currentQuestion = ()
 let acceptingAnswers = true
+let questionCounter = 0
+let availableQuestions []
+
+let questions = [
+    {
+        question: "what is JavaScript?"
+        choice1: "the code used to structure a web page and it's content"
+        choice2: "a programming language that adds interactivity to a website"
+        choice3: "the code that styles web content"
+    }
+    {
+        question: "which is used to separate JavaScript statements?"
+        choice1: "period"
+        choice2: "semicolon"
+        choice3: "colon"
+    }
+    {
+        question: "what is JavaScript abbreviated to?"
+        choice1: "JS"
+        choice2: "J"
+        choice3: "JST"
+    }
+    {
+        question: "which HTML element is used to embed JavaScript code?"
+        choice1: "<a>"
+        choice2: "<link>"
+        choice3: "<script>"
+    }
+    {
+        question: "which is a container thay stores a value?"
+        choice1: "variable"
+        choice2: "function"
+        choice3: "string"
+    }
+]
