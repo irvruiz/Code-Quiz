97136bc8c69ad542ef3734de7d23e91c56c75937
Irvin Ruiz <irvinruiz44@yahoo.com>
2021-09-26 18:12:17 -0600

    added js

---
+++ script.js
@@ -0,0 +1,8 @@
+const question = document.querySelector('#question')
+const choices = array.from(querySelectorAll('.choice-text'))
+const progressText = document.querySelector('#progressText')
+const scoreText = document.querySelector('#score')
+const progressBarrFull = document.querySelector('#progressBarFull')
+
+let currentQuestion = ()
+let acceptingAnswers = true
